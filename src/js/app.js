feef25044bbcdf72097731a5f38afd6e2d35d79d
ilyawebdevelop <ilia966@tut.by>
2025-08-01 16:07:18 +0300

    asd

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,14 @@
 import * as flsFunctions from "./modules/functions.js";
 import "./modules/jquery-3.7.1.min.js";
-import "./modules/fslightbox.js";
+
+import { Fancybox } from "./modules/fancybox.esm.js";
 import "./modules/bootstrap.bundle.min.js";
 import './components.js';
+
+Fancybox.bind("[data-fancybox]", {
+	closeButton: true,
+	showClass: "f-scaleOut",
+});
 
 flsFunctions.isWebp();
 
@@ -10,6 +16,64 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar } from 'swiper';
 Swiper.use([Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar]);
 
+// Инициализация слайдера product-slider
+const productSlider = document.querySelector('.product-slider');
+var mySwiperProductSlider = new Swiper(productSlider, {
+	slidesPerView: 1,
+	speed: 800,
+	spaceBetween: 10,
+	effect: 'fade',
+	autoplay: {
+		delay: 5000,
+	},
+	fadeEffect: {
+		crossFade: true
+	},
+	autoHeight: true,
+	navigation: {
+		prevEl: productSlider.querySelector('.navArrowPrev'),
+		nextEl: productSlider.querySelector('.navArrowNext'),
+	},
+	pagination: {
+		el: document.querySelector('.product-slider .swiper-pagination'),
+		clickable: true,
+		type: 'bullets',
+	},
+});
+
+// Инициализация слайдера productCardSlider
+document.querySelectorAll('.productCardSlider').forEach(n => {
+	const mySwiperProductCard = new Swiper(n, {
+		slidesPerView: 6,
+		spaceBetween: 22,
+		speed: 600,
+		loop: false,
+		navigation: {
+			prevEl: n.closest('.sliderW').querySelector('.navArrowPrev'),
+			nextEl: n.closest('.sliderW').querySelector('.navArrowNext'),
+		},
+		breakpoints: {
+			0: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			768: {
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+			992: {
+				slidesPerView: 4,
+				spaceBetween: 22,
+			},
+			1200: {
+				slidesPerView: 5,
+			},
+			1400: {
+				slidesPerView: 6,
+			}
+		},
+	});
+});
 
 
 
